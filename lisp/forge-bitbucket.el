9073d87cf37aa9ea5c733bf1d8c028ccc2cb88c7
Ola Nilsson <ola.nilsson@gmail.com>
2019-09-07 20:30:01 +0200

    Create topic comments for bitbucket

--- lisp/forge-bitbucket.el
+++ lisp/forge-bitbucket.el
@@ -257,6 +257,19 @@ Callback function CB should accept itself as argument."
                             .id)))
                   data))))
 
+;;; Mutations
+
+(cl-defmethod forge--submit-create-post ((_repo forge-bitbucket-repository) topic)
+  "Create a post in REPO for TOPIC."
+  ;; checkdoc-params: (forge-bitbucket-repository)
+  (forge--buck-post topic
+                    (if (forge-issue-p topic)
+                        "/repositories/:project/issues/:number/comments/"
+                      "/repositories/:project/pullrequests/:number/comments/")
+                    nil
+                    :payload `((content . ((raw . ,(string-trim (buffer-string))))))
+                    :callback  (forge--post-submit-callback)
+                    :errorback (forge--post-submit-errorback)))
 
 ;;; Utilities
 
